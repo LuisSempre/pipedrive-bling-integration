85fd5e5ddb9fda5f9d804a65114b8415561acd1f
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-28 20:46:30 -0300

    Change export const to module.exports

--- src/config/pipedrive.js
+++ src/config/pipedrive.js
@@ -1,1 +1,1 @@
-export const pipedrive_api_url = `https://${process.env.PIPEDRIVE_COMPANY_DOMAIN}.pipedrive.com/api/v1`
+module.exports = pipedrive_api_url = `https://${process.env.PIPEDRIVE_COMPANY_DOMAIN}.pipedrive.com/api/v1`;
