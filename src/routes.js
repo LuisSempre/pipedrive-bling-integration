3344b9772a95c273cc0401cca66e6c520d48d70c
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-28 20:45:39 -0300

    Add List Won Deals

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const pipedrive_api_url = require('./config/pipedrive');
 
-require('dotenv').config();
-
-const companyDomain = process.env.PIPEDRIVE_COMPANY_DOMAIN;
 const pipedriveAPIToken = process.env.PIPEDRIVE_API_TOKEN;
 
 router.get('/deals', async (req, res) => {
   try {
-    const response = await axios.get(`https://${companyDomain}.pipedrive.com/api/v1/deals?api_token=${pipedriveAPIToken}`);
-    // console.log(response.data)
+    const response = await axios.get(`${pipedrive_api_url}/deals?api_token=${pipedriveAPIToken}`);
     return res.json(response.data);
   } catch(error) {
     console.log(error);
+    return res.status(400).json({ message: 'Error' });
   };
 });
+
+router.get('/deals/won', async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&api_token=${pipedriveAPIToken}`);
+    return res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'Error' });
+  }
+})
 
 module.exports = router;
