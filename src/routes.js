6ab139847d72f5c467a35301e668bc4d28b4514b
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-28 22:38:35 -0300

    Add ListAllDeals and ListWonDeals as classes

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-const pipedrive_api_url = require('./config/pipedrive');
+const ListAllDeals = require('./services/ListAllDeals');
+const ListWonDeals = require('./services/ListWonDeals');
 
 const pipedriveAPIToken = process.env.PIPEDRIVE_API_TOKEN;
 
-router.get('/deals', async (req, res) => {
-  try {
-    const response = await axios.get(`${pipedrive_api_url}/deals?api_token=${pipedriveAPIToken}`);
-    return res.json(response.data);
-  } catch(error) {
-    console.log(error);
-    return res.status(400).json({ message: 'Error' });
-  };
-});
+router.get('/deals', ListAllDeals.store);
 
-router.get('/deals/won', async (req, res) => {
-  try {
-    const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&api_token=${pipedriveAPIToken}`);
-    return res.json(response.data);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: 'Error' });
-  }
-})
+router.get('/deals/won', ListWonDeals.store);
+
+
 
 module.exports = router;
