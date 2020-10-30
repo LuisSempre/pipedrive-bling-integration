c981c463bb2a9dc535ac2a736d75dc49968313ea
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 02:20:40 -0300

    Add orders route to Create Order

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const ListAllDealsController = require('./controllers/ListAllDealsController');
 const ListAllOrdersController = require('./controllers/ListAllOrdersController');
 const ListWonDealsController = require('./controllers/ListWonDealsController');
+const CreateOrderController = require('./controllers/CreateOrderController');
 
-router.get('/deals', ListAllDealsController.store);
-router.get('/deals/won', ListWonDealsController.store);
+router.get('/deals', ListAllDealsController.index);
+router.get('/deals/won', ListWonDealsController.index);
 
-router.get('/orders', ListAllOrdersController.store);
+router.get('/orders', ListAllOrdersController.index);
+router.post('/orders', CreateOrderController.create);
 
 module.exports = router;
