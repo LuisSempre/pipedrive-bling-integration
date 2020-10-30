753fd9a784d0d3046a856ba8f3ee5d8a4ee83692
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 00:12:23 -0300

    Remove unnecessary import

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const ListAllDealsController = require('./controllers/ListAllDealsController');
 const ListWonDealsController = require('./controllers/ListWonDealsController');
 
-const pipedriveAPIToken = process.env.PIPEDRIVE_API_TOKEN;
-
 router.get('/deals', ListAllDealsController.store);
-
 router.get('/deals/won', ListWonDealsController.store);
-
-
 
 module.exports = router;
