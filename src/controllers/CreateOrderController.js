704fbabd57e9a42b01a2fd378fd79b7528d565ec
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 02:20:09 -0300

    Add Create Order Controller

---
+++ src/controllers/CreateOrderController.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const orderRepository = require('../repositories/orderRepository');
+const CreateOrderService = require('../services/createOrder');
+const ListWonDealsController = require('./listWonDealsController');
+
+class CreateOrderController {
+  async create(req, res) {
+    try {
+      const response = await ListWonDealsController.getWonDeals();
+      
+      const orders = await CreateOrderService.create(response);
+
+      await orderRepository.saveOrder(orders);
+
+      return res.json(orders);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: 'Error' });
+    };
+  };
+};
+
+module.exports = new CreateOrderController();
