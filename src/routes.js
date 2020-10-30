d1005fff5b497277e005a216bafbf173c868bd38
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 01:43:51 -0300

    Add /orders route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ListAllDealsController = require('./controllers/ListAllDealsController');
+const ListAllOrdersController = require('./controllers/ListAllOrdersController');
 const ListWonDealsController = require('./controllers/ListWonDealsController');
 
 router.get('/deals', ListAllDealsController.store);
 router.get('/deals/won', ListWonDealsController.store);
+
+router.get('/orders', ListAllOrdersController.store);
 
 module.exports = router;
