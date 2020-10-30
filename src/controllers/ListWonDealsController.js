19c171ecf6c51e3416c7a0aef3e92f029caa6aec
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 02:21:58 -0300

    Change controller action to create, for best practices

--- src/controllers/ListWonDealsController.js
+++ src/controllers/ListWonDealsController.js
@@ -5,21 +5,29 @@ const CreateOrderService = require('../services/createOrder');
 const pipedriveAPIToken = process.env.PIPEDRIVE_API_TOKEN;
 
 class ListWonDealsController {
-  async store(req, res) {
+  async index(req, res) {
     try {
       const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&start=0&api_token=${pipedriveAPIToken}`);
-
       const { data } = response.data;
-      const orders = await CreateOrderService.create(data);
 
-      await orderRepository.saveOrder(orders);
-
-      return res.json(orders);
+      return res.json(data);
     } catch (error) {
       console.log(error);
       return res.status(400).json({ message: 'Error' });
     };
   };
+
+  async getWonDeals() {
+    try {
+      const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&start=0&api_token=${pipedriveAPIToken}`);
+      const { data } = response.data;
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: 'Error' });
+    };
+  }
 };
 
 module.exports = new ListWonDealsController();
