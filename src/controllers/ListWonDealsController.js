63ef84f582001b3a682a313bc7fc4980ee9a90ee
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-30 00:13:18 -0300

    Add Create Bling Order

--- src/controllers/ListWonDealsController.js
+++ src/controllers/ListWonDealsController.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
+const CreateOrderService = require('../services/createOrder');
 
 const pipedriveAPIToken = process.env.PIPEDRIVE_API_TOKEN;
 
 class ListWonDealsController {
   async store(req, res) {
     try {
-      const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&api_token=${pipedriveAPIToken}`);
-      return res.json(response.data);
+      const response = await axios.get(`https://api.pipedrive.com/v1/deals?status=won&start=0&api_token=${pipedriveAPIToken}`);
+
+      const { data } = response.data;
+      const orders = await CreateOrderService.create(data);
+
+      return res.json(orders);
     } catch (error) {
       console.log(error);
       return res.status(400).json({ message: 'Error' });
